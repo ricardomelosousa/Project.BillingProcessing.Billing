891603a7d41e0ca6b776755ed8e570fe836c1cd4
ricardomelosousa <ricardo_melo_sousa@hotmail.com>
2022-08-10 01:17:39 -0300

    Delete Project.BillingProcessing.Billing.Domain directory
    ajustes

--- Project.BillingProcessing.Billing.Domain/SeedWork/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Project.BillingProcessing.Customer.Domain.SeedWork
-{
-    public interface IUnitOfWork : IDisposable
-    {
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
-        Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken));
-    }
-}
